672471ea5f0aa27a35fe2b733bd0edc9e6dddf30
mass828 <45210963+mass828@users.noreply.github.com>
2019-01-10 11:10:50 +0000

    Update mysql_pipeline.py

--- mysql_pipeline.py
+++ mysql_pipeline.py
@@ -535,7 +535,7 @@ class MYSQL_Pipeline(object):
           print(Fore.RED + 'TWEET LOG SUCCESS: ' + DATE + ' EST ' + item['name'] + ' ' + item['link'] + Style.RESET_ALL if TWEET.status_code == 200 else Fore.RED + 'TWEET LOG FAILURE: FAILED TO TWEET' + Style.RESET_ALL)
           
           # WebHook for Discord. Comment/Uncomment the line below to enable/disable.
-           requests.post('https://discordapp.com/api/webhooks/524588407330963476/plZQ0nqmP3DxWjLUraotFQhTpVeTxmnVJE1KrDSEeAKADWlUYPR9c5-WnzbdtyFZr5wv', data={'content': "**" + item['name'] + "**" + "\n" + item['link'] + "\n" + "\n" + "[ATC]: " + item['size'] + "\n" + "------------" + "\n"})
+          requests.post('https://discordapp.com/api/webhooks/524588407330963476/plZQ0nqmP3DxWjLUraotFQhTpVeTxmnVJE1KrDSEeAKADWlUYPR9c5-WnzbdtyFZr5wv', data={'content': "**" + item['name'] + "**" + "\n" + item['link'] + "\n" + "\n" + "[ATC]: " + item['size'] + "\n" + "------------" + "\n"})
           
           # WebHook for Slack. Comment/Uncomment the line below to enable/disable.
           # requests.post('SLACK WEBHOOK URL', json={'text': "*" + item['name'] + "*" + "\n" + item['link'] + "\n" + "\n" + "[ATC]: " + item['size'] + "\n" + "------------" + "\n"}, headers={'Content-Type': 'application/json'})
